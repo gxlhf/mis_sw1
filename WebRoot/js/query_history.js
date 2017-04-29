7325868bf59ac3f1f4ce898200199da2f0b1b938
gxlhf <gx.lhf@163.com>
2017-04-29 23:56:28 +0800

    患者详情加入异步获取json

---
+++ WebRoot/js/query_history.js
@@ -0,0 +1,44 @@
+var queryHistoryRecord = {
+  data: [
+    {
+      index: 0,
+      dataID: "123",
+      sex: "男",
+      ageArea: "10-15",
+      examType: "CT",
+      labType: "血常规",
+      queryTime: "2017-4-1"
+    },
+    {
+      index: 1,
+      dataID: "123",
+      sex: "男",
+      ageArea: "15-20",
+      examType: "CT",
+      labType: "血常规",
+      queryTime: "2017-4-1"
+    }
+  ]
+};
+
+var queryHistoryRecord_vue = new Vue({
+  el: "#tb-queryHistoryRecord",
+  data:{
+    tableContent: {
+      data:[
+        {
+          index: 0,
+          dataID: "",
+          sex: "",
+          ageArea: "",
+          examType: "",
+          labType: "",
+          queryTime: ""
+        }
+      ]
+    }
+  },
+  created: function () {
+    this.tableContent = queryHistoryRecord;
+  }
+})
