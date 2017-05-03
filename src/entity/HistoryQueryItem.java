c6e29d7d8c74b7d67c88ac76f99f947e8629e8cb
lww <1510825910@qq.com>
2017-05-03 21:13:28 +0800

    historyqueryitem.java

--- src/entity/HistoryQueryItem.java
+++ src/entity/HistoryQueryItem.java
@@ -4,5 +4,56 @@
 package entity;
 
 public class HistoryQueryItem {
-
+	String sex;
+	int minAge;
+	int maxAge;
+	String examClass;
+	String testItem;
+	Patient[] patients;
+	public HistoryQueryItem(String sex, int minAge, int maxAge, String examClass, String testItem, Patient[] patients) {
+		super();
+		this.sex = sex;
+		this.minAge = minAge;
+		this.maxAge = maxAge;
+		this.examClass = examClass;
+		this.testItem = testItem;
+		this.patients = patients;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getMinAge() {
+		return minAge;
+	}
+	public void setMinAge(int minAge) {
+		this.minAge = minAge;
+	}
+	public int getMaxAge() {
+		return maxAge;
+	}
+	public void setMaxAge(int maxAge) {
+		this.maxAge = maxAge;
+	}
+	public String getExamClass() {
+		return examClass;
+	}
+	public void setExamClass(String examClass) {
+		this.examClass = examClass;
+	}
+	public String getTestItem() {
+		return testItem;
+	}
+	public void setTestItem(String testItem) {
+		this.testItem = testItem;
+	}
+	public Patient[] getPatients() {
+		return patients;
+	}
+	public void setPatients(Patient[] patients) {
+		this.patients = patients;
+	}
+	
 }
